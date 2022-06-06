ecb38b68b11680bc708bd0b328c52792ac39af95
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-06 09:58:50 -0700

    All alx 0x01 tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Prints numbers between 00 to 99.
  *
@@ -11,19 +10,15 @@ int main(void)
 int i, e, g, h, op1, op2;
 
 i = e = g = h = 48;
-
 while (h < 58)
 {
 	g = 48;
-
 	while (g < 58)
 	{
 		e = 48;
-
 		while (e < 58)
 		{
 			i = 48;
-
 			while (i < 58)
 			{
 				op1 = (h * 10) + g;
@@ -36,9 +31,7 @@ while (h < 58)
 					putchar(e);
 					putchar(i);
 					if (h == 57 && g == 56 && e == 57 && i == 57)
-
 						break;
-
 					putchar(',');
 					putchar(' ');
 				}
