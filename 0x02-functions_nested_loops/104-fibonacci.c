ac2ec488e9090a4edb0e62e7775da0a53a58fa1d
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-07 12:45:12 -0700

    Add 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,17 @@
  */
 int main(void)
 {
-	int c, boolean, boolean2;
+	int c, bool_1, bool_2;
 
 	long int n1, n2, fn, fn2, n11, n22;
 
-
 	n1 = 1;
 	n2 = 2;
-	boolean =  boolean2 = 1;
+	bool_1 =  bool_2 = 1;
 	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
-		if (boolean)
+		if (bool_1)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
@@ -27,13 +26,13 @@ int main(void)
 		}
 		else
 		{
-			if (boolean2)
+			if (bool_2)
 			{
 				n11 = n1 % 1000000000;
 				n22 = n2 % 1000000000;
 				n1 = n1 / 1000000000;
 				n2 = n2 / 1000000000;
-				boolean2 = 0;
+				bool_2 = 0;
 			}
 			fn2 = (n11 + n22);
 			fn = n1 + n2 + (fn2 / 1000000000);
@@ -44,8 +43,8 @@ int main(void)
 			n2 = fn;
 			n22 = (fn2 % 1000000000);
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		if (((n1 + n2) < 0) && bool_1 == 1)
+			bool_1 = 0;
 	}
 	printf("\n");
 	return (0);
