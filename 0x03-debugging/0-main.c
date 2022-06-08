ba3ad4f2a4542347be08cb653aa214f3660545e6
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-08 06:27:51 -0700

    Add 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * postive_or_negative - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
