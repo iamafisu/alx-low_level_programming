b92faf4438a24ac254ecf088584a17ce66cc5972
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-15 15:58:32 -0700

    Add 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings,
- * 
+ * @dest: destination.
+ * @src: source.
  * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
