081312fbfd3daf3a6aaed8d817753f31272c4b05
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-28 09:06:47 -0700

    All alx 0x0B project task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- *
  * create_array - creates an array of chars.
  * @size: size of the array.
  * @c: storaged char
  *
  * Return: pointer of an array of chars
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *cr;
