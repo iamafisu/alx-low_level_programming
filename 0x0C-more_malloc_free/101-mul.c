1570b5aa0e0a736825f1ffe322790157c9aca435
Afisu Ganiyu <afisu.g@hotmail.com>
2022-06-30 12:48:39 -0700

    All alx 0x0C Project task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,6 +9,7 @@
  *
  * Return: no return.
  */
+
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
@@ -41,6 +42,7 @@ void _is_zero(char *argv[])
  *
  * Return: pointer of a char array.
  */
+
 char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
@@ -81,6 +83,7 @@ int _checknum(char *argv[], int n)
  *
  * Return: 0 - success.
  */
+
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
